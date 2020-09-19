f030e20594b53c99acab65c29008d904362b9835
99002477 <71481753+99002477@users.noreply.github.com>
2020-09-19 10:58:27 +0530

    Update header.h

--- header.h
+++ header.h
@@ -1,7 +1,12 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-
+struct book{
+	char code[20];
+	char name[20];
+	char date[20];
+	int cost;
+}b;
 void login();
 void insert_details();
 void view_All();
